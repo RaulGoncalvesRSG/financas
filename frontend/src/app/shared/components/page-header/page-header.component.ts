05e0800be498e6dcb6afd4de2eab0373eb2af20f
Raul Gonçalves <raulsantos121@gmail.com>
2022-02-11 11:42:35 -0300

    PageHeaderComponent

---
+++ frontend/src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+
+  //O nome da propriedade dentro do input é oq o usuário irá passar como parâmetro. Ex: <tag [page-title] = "valor"/>
+  @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton: boolean = true;
+  @Input('button-class') buttonClass: string;
+  @Input('button-text') buttonText: string;
+  @Input('button-link') buttonLink: string;       //Para onde o usuário será redirecionado qnd clicar
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
